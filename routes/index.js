bfb10d7b974459674a400ed35c241910d45c0389
xiaowei <1412157658@qq.com>
2019-07-04 19:18:13 +0800

    修改了一些bug

--- routes/index.js
+++ routes/index.js
@@ -156,85 +156,52 @@ router.get('/userlist', function(req, res) {
   })
 })
 
-// 获取当前用户所有相关的聊天信息列表
-router.get('/msglist', function(req, res){
+router.get('/msglist', function (req, res) {
   // 获取cookie中的userid
   const userid = req.cookies.userid
-  // 查询得到的所有user文档数组
-  UserModel.find(function(err, userDocs) {
-    // 用对象储存所有user信息：key为user的_id，val为name和header组成的user对象
-    // const users = {} // 对象容器
-    // userDocs.forEach(doc => {
-    //   user[doc._id] = {
-    //     username: doc.username,
-    //     header: doc.header
-    //   }
-    // })
+  // 查询得到所有user文档数组
+  UserModel.find(function (err, userDocs) {
+    // 用对象存储所有user信息: key为user的_id, val为name和header组成的user对象
+    /*const users = {} // 对象容器
+    userDocs.forEach(doc => {
+      users[doc._id] = {username: doc.username, header: doc.header}
+    })*/
 
     const users = userDocs.reduce((users, user) => {
-      users[user._id] = {
-        username: user.username,
-        header: user.header
-      }
+      users[user._id] = {username: user.username, header: user.header}
       return users
-    }, {})
-    // 返回结构
-    /**{
-     *   _id:{
-     *        username: xxx,
-     *        header: xxx
-     *    },
-     *   _id:{
-     *        username: xxx,
-     *        header: xxx
-     *    },
-     * }
-     *
-     */
-
-    /**
-     * 查询userid相关的所有聊天信息
-     * find ()
-     *
-     * 参数1：查询条件
-     * 参数2：过滤条件
-     * 参数3：回调函数
-     *
-     */
-    ChatModel.find({'$or': [
-      {from: userid},
-      {to: userid}
-    ]},filter,function(err, chatMsgs){
-      // 返回包含所有用户和当前用户相关的所有聊天信息的数据
+    } , {})
+    /*
+    查询userid相关的所有聊天信息
+     参数1: 查询条件
+     参数2: 过滤条件
+     参数3: 回调函数
+    */
+    ChatModel.find({'$or': [{from: userid}, {to: userid}]}, filter, function (err, chatMsgs) {
+      // 返回包含所有用户和当前用户相关的所有聊天消息的数据
       res.send({code: 0, data: {users, chatMsgs}})
     })
   })
-
 })
 
-/**
- * 修改制定消息为已读
+/*
+修改指定消息为已读
  */
-router.post('/readmsg', function(req, res) {
+router.post('/readmsg', function (req, res) {
+  // 得到请求中的from和to
   const from = req.body.from
   const to = req.cookies.userid
-  /**
-   * 更新数据库中的chat数据
-   * update()
-   *
-   * 参数1：查询条件
-   * 参数2：更新为指定的数据对象
-   * 参数3：是否为1次更新多条，默认只更新一条
-   * 参数4：更新完的回调函数
+  /*
+  更新数据库中的chat数据
+  参数1: 查询条件
+  参数2: 更新为指定的数据对象
+  参数3: 是否1次更新多条, 默认只更新一条
+  参数4: 更新完成的回调函数
    */
-  ChatModel.update({from, to, read: false}, {read: true}, {multi: true}, function(err, doc){
-    // console.log('/readmsg', doc)
-    res.send({
-      code: 0,
-      data: data.nModified
-    })// 更新的数量
+  ChatModel.update({from, to, read: false}, {read: true}, {multi: true}, function (err, doc) {
+    console.log('/readmsg', doc)
+    res.send({code: 0, data: doc.nModified}) // 更新的数量
   })
-
 })
 
 module.exports = router;
